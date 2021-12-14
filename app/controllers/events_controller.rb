a40ce9fdc2bceddfa004d360849cf86356ecafb0
seeleng <seelengxd@gmail.com>
2021-12-14 19:40:55 +0800

    Add create event and table styles

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,25 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to current_user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :location)
   end
 end
